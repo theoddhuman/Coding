5cc0c7bcb456cff09aa37c6489a14d2dfe403fbd
theoddhuman <subhampaul137@gmail.com>
2023-01-22 19:30:59 +0530

    Single Linked List Implementation

---
+++ src/com/paul/subham/linkedlist/LinkedListTest.java
@@ -0,0 +1,23 @@
+package com.paul.subham.linkedlist;
+
+import com.paul.subham.linkedlist.implementation.single.LinkedList;
+import com.paul.subham.linkedlist.implementation.single.Node;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = new Node(1);
+        linkedList.head.next = new Node(2);
+        linkedList.head.next.next = new Node(3);
+        //linkedList.insertAfterNode(linkedList.head, 4);
+        //linkedList.insertAtMiddle(7,3);
+        //linkedList.insertAtStart(4);
+        //linkedList.insertAtEnd(5);
+        //linkedList.deleteAtStart();
+        //linkedList.deleteAtEnd();
+        //linkedList.deleteAtMiddle(2);
+        //linkedList.deleteFirstOccurence(3);
+        linkedList.delete();
+        linkedList.print();
+    }
+}
---
+++ src/com/paul/subham/linkedlist/implementation/single/LinkedList.java
@@ -0,0 +1,126 @@
+package com.paul.subham.linkedlist.implementation.single;
+
+import com.paul.subham.linkedlist.implementation.single.Node;
+
+/**
+ * 1. insert data at start
+ * 2. insert data at end
+ * 3. insert after Node
+ * 4. insert at middle position
+ * 5. delete at start
+ * 6. delete at end
+ * 7. delete at middle position
+ * 8. delete first occurence
+ * 9. delete list
+ * 10.print linked list
+ */
+public class LinkedList {
+    public Node head;
+
+    //insert data at start
+    public void insertAtStart(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    //insert data at end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while(current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    //insert after Node
+    public void insertAfterNode(Node node, int data) {
+        Node newNode = new Node(data);
+        newNode.next = node.next;
+        node.next = newNode;
+    }
+
+    //insert at middle position
+    public void insertAtMiddle(int data, int position) {
+        Node newNode = new Node(data);
+        Node current = head;
+        for(int i=1; i<position-1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+    //delete at start
+    public void deleteAtStart() {
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+    }
+
+    //delete at end
+    public void deleteAtEnd() {
+        Node temp = head;
+        Node pre = null;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        pre.next = null;
+    }
+
+    //delete at middle position
+    public void deleteAtMiddle(int position) {
+        if(position == 1) {
+            deleteAtStart();
+            return;
+        }
+
+        Node temp = head;
+        Node pre = null;
+        for(int i=0; i< position - 1; i++) {
+            pre = temp;
+            temp = temp.next;
+        }
+        pre.next = temp.next;
+    }
+
+    //delete first occurence
+    public void deleteFirstOccurence(int data) {
+        Node temp = head;
+        Node pre = null;
+        if(temp != null && temp.data == data) {
+            head = temp.next;
+            return;
+        }
+
+        while(temp != null && temp.data != data) {
+            pre =temp;
+            temp = temp.next;
+        }
+        pre.next = temp.next;
+    }
+
+    //delete list
+    public void delete() {
+        head = null;
+    }
+
+    //print linked list
+    public void print() {
+        Node node = head;
+        while(node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
