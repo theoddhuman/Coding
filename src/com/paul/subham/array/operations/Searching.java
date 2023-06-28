b626906494d384eb06cd51f677efb2d9a6f9d01b
theoddhuman <subhampaul137@gmail.com>
2023-06-28 21:34:49 +0530

    Array duplicate and repeat elements

--- src/com/paul/subham/array/operations/Searching.java
+++ src/com/paul/subham/array/operations/Searching.java
@@ -1,22 +1,23 @@
 package com.paul.subham.array.operations;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
- * 1. Find duplicate in an array (Using sorting)
- * 2. Find duplicate in an array (Using hashing)
- * 3. Find duplicate in an array (space optimized)
+ * 1. Finding duplicate in an array (Using sorting)
+ * 2. Finding duplicate in an array (Using hashing)
+ * 3. Finding duplicate in an array (space optimized)
+ * 4. Finding most appearing element in an array (using hashing)
+ * 5. Finding most appearing element in an array (space optimized)
+ * 6. Finding first repeating element in an array (using hashing)
  */
 public class Searching {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,3,5};
-        System.out.println(findDuplicateAllPositive(a));
+        int[] a = {1,2,3,4,2, 3,5, 6, 4, 3, 2, 1, 2,2};
+        System.out.println(getFirstRepeatingElement(a));
     }
 
     /**
-     * Find duplicate in an array (Using sorting)
+     * Finding duplicate in an array (Using sorting)
      * TC: O(nlogn)
      * SC: O(1)
      */
@@ -31,7 +32,7 @@ public class Searching {
     }
 
     /**
-     * Find duplicate in an array (Using hashing)
+     * Finding duplicate in an array (Using hashing)
      * TC: O(n)
      * SC: O(n)
      */
@@ -48,7 +49,7 @@ public class Searching {
     }
 
     /**
-     * Find duplicate in an array (space optimized)
+     * Finding duplicate in an array (space optimized)
      * All positive elements and ranges from 0 to n-1
      * TC: O(n)
      * SC: O(1)
@@ -61,5 +62,63 @@ public class Searching {
             a[Math.abs(a[i])] = -a[Math.abs(a[i])];
         }
         return false;
+    }
+
+    /**
+     * Finding most appearing element in an array (using hashing)
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int getMostAppearingElement(int[] a) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for(int i=0; i<a.length; i++) {
+            if(countMap.containsKey(a[i])) {
+                countMap.put(a[i],countMap.get(a[i])+1);
+            } else {
+                countMap.put(a[i], 1);
+            }
+        }
+        Optional<Map.Entry<Integer, Integer>> max = countMap.entrySet().stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue));
+        return max.get().getKey();
+    }
+
+    /**
+     * Finding most appearing element in an array (space optimized)
+     * All positive elements and ranges from 0 to n-1
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public static int getMostAppearingElementSpaceOptimized(int[] a) {
+        int n = a.length;
+        for(int i=0; i<n; i++) {
+            a[a[i]%n] += n;
+        }
+        int max=0;
+        int maxElement=0;
+        for(int i=0; i<n; i++) {
+            if(a[i]/n > max) {
+                max = a[i] / n;
+                maxElement = i;
+            }
+        }
+        return maxElement;
+    }
+
+    /**
+     * Finding first repeating element in an array (using hashing)
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int getFirstRepeatingElement(int[] a){
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<a.length; i++) {
+            if(set.contains(a[i])) {
+                return a[i];
+            } else {
+                set.add(a[i]);
+            }
+        }
+        return -1;
     }
 }
