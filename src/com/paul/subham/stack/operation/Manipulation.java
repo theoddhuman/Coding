1750bfd0c354e20780123269b251238b6eb2bdcb
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-02-06 23:13:32 +0530

    sort a stack

--- src/com/paul/subham/stack/operation/Manipulation.java
+++ src/com/paul/subham/stack/operation/Manipulation.java
@@ -7,20 +7,32 @@ import java.util.Stack;
 
 /**
  * @author subham.paul
- * 
+ *
  * 1. Reverse a stack (By recursion)
  * 2. Reverse a stack (Efficient Approach)
+ * 3. Sort a stack (by recursion)
+ * 4. Sort a stack (by temporary stack)
  */
 public class Manipulation {
     public static void main(String[] args) {
-        LinkedListStack stack = new LinkedListStack();
-        stack.push(1);
-        stack.push(2);
+//        LinkedListStack stack = new LinkedListStack();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        stack.push(4);
+//        stack.print();
+//        reverseEfficient(stack);
+//        stack.print();
+
+        Stack<Integer> stack = new Stack<>();
         stack.push(3);
-        stack.push(4);
-        stack.print();
-        reverseEfficient(stack);
-        stack.print();
+        stack.push(2);
+        stack.push(8);
+        stack.push(1);
+        stack.push(5);
+        System.out.println(stack);
+        stack = sortByTemporaryStack(stack);
+        System.out.println(stack);
     }
 
     /**
@@ -50,9 +62,9 @@ public class Manipulation {
 
     /**
      * Reverse a stack (Efficient Approach)
-     * 
+     *
      * Stack has to be implemented using linked list
-     * 
+     *
      * TC: O(n)
      * SC: O(1)
      */
@@ -71,5 +83,48 @@ public class Manipulation {
             current = next;
         }
         return prev;
+    }
+
+    /**
+     * Sort a stack (by recursion)
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static void sortByRecursion(Stack<Integer> stack) {
+        if(stack.isEmpty()) {
+            return;
+        }
+        int x = stack.pop();
+        sortByRecursion(stack);
+        sortedInsert(stack, x);
+    }
+
+    private static void sortedInsert(Stack<Integer> stack, int data) {
+        if(stack.isEmpty() || data > stack.peek()) {
+            stack.push(data);
+            return;
+        }
+        int x = stack.pop();
+        sortedInsert(stack, data);
+        stack.push(x);
+    }
+
+    /**
+     * Sort a stack (by temporary stack)
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static Stack<Integer> sortByTemporaryStack(Stack<Integer> stack) {
+        Stack<Integer> sort = new Stack<>();
+        while(!stack.isEmpty()) {
+            int current = stack.pop();
+            while(!sort.isEmpty() && current < sort.peek()) {
+                stack.push(sort.pop());
+            }
+            sort.push(current);
+        }
+        return sort;
     }
 }
