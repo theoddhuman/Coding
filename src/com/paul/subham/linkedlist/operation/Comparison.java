b34d477706f851536dba9c216fc31044ae16c2bb
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-05-01 01:05:08 +0530

    check if a list is circular

--- src/com/paul/subham/linkedlist/operation/Comparison.java
+++ src/com/paul/subham/linkedlist/operation/Comparison.java
@@ -5,18 +5,23 @@ import com.paul.subham.linkedlist.implementation.single.Node;
 
 /**
  * @author subham.paul
+ * 
+ * 1. Check if two linked lists are identical
+ * 2. Check if a linked list is circular linked list
+ * 3. Check if a linked list is circular linked list (Using slow fast pointer)
  */
 public class Comparison {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
         linkedList.insertAtEnd(1);
         linkedList.insertAtEnd(3);
+        linkedList.head.next.next = linkedList.head;
 
         LinkedList linkedList1 = new LinkedList();
         linkedList1.insertAtEnd(1);
         linkedList1.insertAtEnd(2);
 
-        System.out.println(isIdentical(linkedList, linkedList1));
+        System.out.println(isCircularSlowFast(linkedList));
 
     }
 
@@ -37,5 +42,41 @@ public class Comparison {
             temp2 = temp2.next;
         }
         return (temp1 == null) && (temp2 == null);
+    }
+
+    /**
+     * Check if a linked list is circular linked list
+     *
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public static boolean isCircular(LinkedList linkedList) {
+        if(linkedList.head == null) {
+            return true;
+        }
+        Node node = linkedList.head.next;
+        while(node != linkedList.head && node != null) {
+            node = node.next;
+        }
+        return node == linkedList.head;
+    }
+
+    /**
+     * Check if a linked list is circular linked list (Using slow fast pointer)
+     *
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public static boolean isCircularSlowFast(LinkedList linkedList) {
+        Node slow = linkedList.head;
+        Node fast = linkedList.head.next;
+        while(fast != null && fast.next != null) {
+            if(slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
     }
 }
