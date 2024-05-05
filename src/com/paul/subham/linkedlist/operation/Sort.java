04b372330625742f7636037cbaa7bf1cc8f4466c
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-05-05 17:02:20 +0530

    quick sort of dll

--- src/com/paul/subham/linkedlist/operation/Sort.java
+++ src/com/paul/subham/linkedlist/operation/Sort.java
@@ -1,21 +1,26 @@
 package com.paul.subham.linkedlist.operation;
 
+import com.paul.subham.linkedlist.implementation.doubly.DLNode;
+import com.paul.subham.linkedlist.implementation.doubly.DoublyLinkedList;
 import com.paul.subham.linkedlist.implementation.single.LinkedList;
 import com.paul.subham.linkedlist.implementation.single.Node;
 
 /**
  * @author subham.paul
+ * 
+ * 1. Insertion sort of linked list
+ * 2. Quick sort of doubly linked list
  */
 public class Sort {
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
+        DoublyLinkedList linkedList = new DoublyLinkedList();
         linkedList.insertAtEnd(5);
         linkedList.insertAtEnd(4);
         linkedList.insertAtEnd(1);
         linkedList.insertAtEnd(3);
         linkedList.insertAtEnd(2);
         linkedList.print();
-        insertionSort(linkedList);
+        quickSort(linkedList);
         System.out.println();
         linkedList.print();
     }
@@ -23,7 +28,7 @@ public class Sort {
 
     /**
      * Insertion sort of linked list
-     * 
+     *
      * TC: O(n^2)
      * SC: O(1)
      */
@@ -51,5 +56,46 @@ public class Sort {
         }
         temp.next = newNode;
         newNode.next = current;
+    }
+
+    /**
+     * Quick sort of doubly linked list
+     *
+     * TC: O(n^2)
+     * SC: O(1)
+     */
+    private static void quickSort(DoublyLinkedList linkedList) {
+        DLNode current = linkedList.head;
+        DLNode end = null;
+        while(current != null) {
+            end = current;
+            current = current.next;
+        }
+        quickSortUtil(linkedList.head, end);
+    }
+
+    private static void quickSortUtil(DLNode start, DLNode end) {
+        if(end != null && start != end && start != end.next) {
+            DLNode partition = partition(start, end);
+            quickSortUtil(start, partition.pre);
+            quickSortUtil(partition.next, end);
+        }
+    }
+
+     private static DLNode partition(DLNode start, DLNode end) {
+        int pivot = end.data;
+        DLNode pIndex = start;
+        for(DLNode i = start; i != end; i = i.next) {
+            if(i.data <= pivot) {
+                int temp = pIndex.data;
+                pIndex.data = i.data;
+                i.data = temp;
+                pIndex = pIndex.next;
+            }
+        }
+        int temp = pIndex.data;
+        pIndex.data = end.data;
+        end.data = temp;
+        return pIndex;
     }
 }
