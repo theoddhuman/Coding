04fb13fd8438e624d08cca0f764d8632ec47dee0
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-05-14 02:01:26 +0530

    count sibling

--- src/com/paul/subham/tree/operations/generic/Structure.java
+++ src/com/paul/subham/tree/operations/generic/Structure.java
@@ -1,20 +1,45 @@
 package com.paul.subham.tree.operations.generic;
 
+import com.paul.subham.tree.implementation.generic.GNode;
+import com.paul.subham.tree.implementation.generic.GenericTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author subham.paul
- * 
+ *
  * 1. Depth or height of array representation of generic tree
  * 2. Depth or height of array representation of generic tree (Dynamic Programming)
+ * 3. Count siblings of a node in generic tree (Recursive)
+ * 4. Count siblings of a node in generic tree (iterative)
  */
 public class Structure {
     public static void main(String[] args) {
-        int[] a = {-1, 0, 1, 6, 6, 0, 0, 2, 7};
-        System.out.println(depthFromGenericArrayDP(a));
+//        int[] a = {-1, 0, 1, 6, 6, 0, 0, 2, 7};
+//        System.out.println(depthFromGenericArrayDP(a));
+        GenericTree genericTree = new GenericTree();
+        genericTree.root = new GNode(1);
+        GNode gNode = genericTree.addChild(genericTree.root, 2);
+        GNode gNode1 = genericTree.addSibling(gNode, 3);
+        genericTree.addSibling(gNode, 4);
+        GNode gNode2 = genericTree.addChild(gNode, 7);
+        genericTree.addSibling(gNode2, 8);
+        GNode gNode3 = genericTree.addChild(gNode1, 9);
+        GNode gNode4 = genericTree.addSibling(gNode3, 10);
+        genericTree.addSibling(gNode3, 11);
+        GNode gNode5 = genericTree.addChild(gNode4, 12);
+        genericTree.addSibling(gNode5, 14);
+        genericTree.addSibling(gNode5, 15);
+        genericTree.traversal(genericTree.root);
+        System.out.println();
+        System.out.println(countSiblingIterative(genericTree, gNode4));
     }
 
     /**
      * Depth or height of array representation of generic tree
-     * 
+     * {-1, 0, 1, 6, 6, 0, 0, 2, 7}
+     *
      * TC: O(n^2)
      * SC: O(1)
      */
@@ -57,5 +82,75 @@ public class Structure {
             max = Math.max(c[i], max);
         }
         return max;
+    }
+
+    /**
+     * Count siblings of a node in generic tree (Recursive)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int countSiblingRecursive(GenericTree genericTree, GNode sibling) {
+        return countSiblingUtil(genericTree.root, sibling);
+    }
+
+    private static int countSiblingUtil(GNode node, GNode bro) {
+        if(node == null) {
+            return 0;
+        }
+        int count = 0;
+        boolean found = false;
+        int childCount = 0;
+        while(node != null) {
+            count++;
+            if(node == bro) {
+                found = true;
+            }
+            if(node.child != null && !found) {
+                childCount = countSiblingUtil(node.child, bro);
+            }
+            if(childCount > 0) {
+                return childCount;
+            }
+            node = node.next;
+        }
+        if(found) {
+            return count;
+        }
+        return 0;
+    }
+
+    /**
+     * Count siblings of a node in generic tree (iterative)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int countSiblingIterative(GenericTree genericTree, GNode bro) {
+        if(genericTree.root == null) {
+            return 0;
+        }
+        Queue<GNode> queue = new LinkedList<>();
+        GNode current = null;
+        queue.add(genericTree.root);
+        while(!queue.isEmpty()) {
+            current = queue.remove();
+            int count=0;
+            boolean found = false;
+            while(current != null) {
+                count++;
+                if(current == bro) {
+                    found = true;
+                }
+                if(current.child != null) {
+                    queue.add(current.child);
+                }
+                current = current.next;
+            }
+            if(found) {
+                return count;
+            }
+        }
+        return 0;
     }
 }
