2fb7b2faddef99abc6b8bf3b915b6e26c3c17255
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-06-07 22:35:42 +0530

    consruction

--- src/com/paul/subham/tree/operations/Construction.java
+++ src/com/paul/subham/tree/operations/Construction.java
@@ -15,12 +15,18 @@ import java.util.Stack;
  * 1. Construct binary tree from preorder and inorder traversal
  * 2. Construct binary tree from preorder and inorder traversal (Using hashing)
  * 3. Construct binary tree from preorder and inorder traversal (using stack and set)
+ * 4. Construct binary tree from postorder and inorder traversal
+ * 5. Construct binary tree from postorder and inorder traversal (Hashing)
+ * 6. Construct binary tree from levelorder and inorder traversal
+ * 7. Construct binary tree from levelorder and inorder traversal (Hashing)
  */
 public class Construction {
     public static void main(String[] args) {
-         int[] pre = {1,2,4,5,6,7,3};
-         int[] in = {4,2,6,5,7,1,3};
-         BinaryTree binaryTree = constructBinaryTreeStackAndSet(pre, in);
+         //int[] pre = {1,2,4,5,6,7,3};
+         //int[] post = {4,5,2,6,7,3,1};
+         int[] in = {4,2,5,1,6,3,7};
+         int[] level = {1,2,3,4,5,6,7};
+         BinaryTree binaryTree = constructBinaryTreeLevelInHashing(level, in);
          binaryTree.levelOrder();
     }
 
@@ -138,6 +144,141 @@ public class Construction {
             }
         }
         return binaryTree;
+    }
+
+    /**
+     * Construct binary tree from postorder and inorder traversal
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreePostIn(int[] in, int[] post) {
+        BinaryTree binaryTree = new BinaryTree();
+        pIndex = post.length-1;
+        binaryTree.root = buildBTPost(post, in, 0, in.length-1);
+        return binaryTree;
+    }
+
+    private static Node buildBTPost(int[] post, int[] in, int inStart, int inEnd) {
+        if(inStart > inEnd) {
+            return null;
+        }
+        Node node = new Node(post[pIndex--]);
+        if(inStart == inEnd) {
+            return node;
+        }
+        int index = search(in, inStart, inEnd, node.data);
+        node.right = buildBTPost(post, in, index+1, inEnd);
+        node.left = buildBTPost(post, in, inStart, index-1);
+        return node;
+    }
+
+    /**
+     * Construct binary tree from postorder and inorder traversal (Hashing)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreePostInHashing(int[] in, int[] post) {
+        BinaryTree binaryTree = new BinaryTree();
+        pIndex = post.length-1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<in.length; i++) {
+            map.put(in[i], i);
+        }
+        binaryTree.root = buildBTPost(post, in, 0, in.length-1, map);
+        return binaryTree;
+    }
+
+    private static Node buildBTPost(int[] post, int[] in, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if(inStart > inEnd) {
+            return null;
+        }
+        Node node = new Node(post[pIndex--]);
+        if(inStart == inEnd) {
+            return node;
+        }
+        int index = inMap.get(node.data);
+        node.right = buildBTPost(post, in, index+1, inEnd, inMap);
+        node.left = buildBTPost(post, in, inStart, index-1, inMap);
+        return node;
+    }
+
+    /**
+     * Construct binary tree from levelorder and inorder traversal
+     *
+     * TC: O(n^3)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeLevelIn(int[] level, int[] in) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = buildBinaryTree(level, in, 0, in.length-1);
+        return binaryTree;
+    }
+
+    private static Node buildBinaryTree(int[] level, int[] in, int inStart, int inEnd) {
+        if(inStart > inEnd) {
+            return null;
+        }
+        if(inStart == inEnd) {
+            return new Node(in[inStart]);
+        }
+        Node node = null;
+        int index = 0;
+        for(int i=0; i<level.length; i++) {
+            boolean found = false;
+            for(int j=inStart; j<=inEnd; j++) {
+                if(level[i] == in[j]) {
+                    node = new Node(in[j]);
+                    index = j;
+                    found = true;
+                    break;
+                }
+            }
+            if(found) {
+                break;
+            }
+        }
+        node.left = buildBinaryTree(level, in, inStart, index-1);
+        node.right = buildBinaryTree(level, in, index+1, inEnd);
+        return node;
+    }
+
+    /**
+     * Construct binary tree from levelorder and inorder traversal (Hashing)
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeLevelInHashing(int[] level, int[] in) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = buildBinaryTreeHashing(level, in, 0, in.length-1);
+        return binaryTree;
+    }
+
+    private static Node buildBinaryTreeHashing(int[] level, int[] in, int inStart, int inEnd) {
+        if(inStart > inEnd) {
+            return null;
+        }
+        if(inStart == inEnd) {
+            return new Node(in[inStart]);
+        }
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for(int i=inStart; i<=inEnd; i++) {
+            inMap.put(in[i], i);
+        }
+        Node node = null;
+        int index = 0;
+        for(int i=0; i<level.length; i++) {
+            if(inMap.containsKey(level[i])) {
+                node = new Node(level[i]);
+                index = inMap.get(level[i]);
+                break;
+            }
+        }
+        node.left = buildBinaryTree(level, in, inStart, index-1);
+        node.right = buildBinaryTree(level, in, index+1, inEnd);
+        return node;
     }
 
 }
