c1cc75e1ce82941e6c91ced0160bfbbe8ca8c03a
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-07-24 21:18:02 +0530

    min stack

--- src/com/paul/subham/stack/implementation/MinStack.java
+++ src/com/paul/subham/stack/implementation/MinStack.java
@@ -6,15 +6,38 @@ import java.util.Stack;
  * @author subham.paul
  *
  * Implementation of stack to get min element at constant time. (Space Optimized)
+ * 
+ * 1. Push a data
+ * 2. Push a data (Space optimized)
+ * 3. Pop a data
+ * 4. Pop a data (Space optimized)
  */
 public class MinStack extends Stack<Integer> {
     Stack<Integer> minStack = new Stack<>();
 
-
     /**
+     * Push a data
+     * 
      * TC: O(1)
+     * SC: O(1)
      */
     void push(int x) {
+        super.push(x);
+        if(super.isEmpty()) {
+            minStack.push(x);
+        } else {
+            super.push(x);
+            int y = minStack.peek();
+            minStack.push(Math.min(x, y));
+        }
+    }
+
+    /**
+     * Push a data (Space optimized)
+     * 
+     * TC: O(1)
+     */
+    void pushSpaceOptimized(int x) {
         if(super.isEmpty()) {
             minStack.push(x);
         } else {
@@ -26,9 +49,26 @@ public class MinStack extends Stack<Integer> {
     }
 
     /**
+     * Pop a data
+     * 
      * TC: O(1)
+     * SC: O(1)
      */
     public Integer pop() {
+        if(super.isEmpty()) {
+            return Integer.MIN_VALUE;
+        }
+        int x = super.pop();
+        minStack.pop();
+        return x;
+    }
+
+    /**
+     * Pop a data (Space optimized)
+     * 
+     * TC: O(1)
+     */
+    public Integer popSpaceOptimized() {
         if(super.isEmpty()) {
             return Integer.MIN_VALUE;
         }
