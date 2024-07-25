b05b97dd7dd312412e5f39ed439b4c673428f03f
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-07-25 11:42:07 +0530

    reverse word

--- src/com/paul/subham/string/operations/Manipulation.java
+++ src/com/paul/subham/string/operations/Manipulation.java
@@ -1,17 +1,22 @@
 package com.paul.subham.string.operations;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 /**
  * @author subham.paul
  *
  * 1. Remove specified characters from a string
+ * 2. Reverse individual words in a string (Using stack)
+ * 3. Reverse individual words in a string (Using Java stream)
  */
 public class Manipulation {
     public static void main(String[] args) {
-        char[] res = removeSpecificChars("hellobaby".toCharArray(), new char[]{'l','o','a'});
-        System.out.println(String.valueOf(res));
+        //char[] res = removeSpecificChars("hellobaby".toCharArray(), new char[]{'l','o','a'});
+        System.out.println(reverseWordA("I am Subham"));
     }
 
     /**
@@ -34,5 +39,40 @@ public class Manipulation {
         char[] res = new char[j];
         System.arraycopy(str, 0, res, 0, j);
         return res;
+    }
+
+    /**
+     * Reverse individual words in a string (Using stack)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static void reverseWord(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) != ' ') {
+                stack.push(s.charAt(i));
+            } else {
+                while(!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(" ");
+            }
+        }
+        while(!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+
+    /**
+     * Reverse individual words in a string (Using Java stream)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static String reverseWordA(String s) {
+        return Arrays.stream(s.split(" "))
+                .map(str -> new StringBuilder(str).reverse())
+                .collect(Collectors.joining(" "));
     }
 }
