955382b6a8afdcc263451bc99aa3019c4966dbf9
Subham Paul <43453545+theoddhuman@users.noreply.github.com>
2024-07-26 15:49:34 +0530

    construction of binary tree

--- src/com/paul/subham/tree/operations/Construction.java
+++ src/com/paul/subham/tree/operations/Construction.java
@@ -1,11 +1,13 @@
 package com.paul.subham.tree.operations;
 
+import com.paul.subham.linkedlist.implementation.single.LinkedList;
 import com.paul.subham.tree.implementation.binary.BinaryTree;
 import com.paul.subham.tree.implementation.binary.Node;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
@@ -19,15 +21,37 @@ import java.util.Stack;
  * 5. Construct binary tree from postorder and inorder traversal (Hashing)
  * 6. Construct binary tree from levelorder and inorder traversal
  * 7. Construct binary tree from levelorder and inorder traversal (Hashing)
+ * 8. Construct binary tree from linked list
+ * 9. Construct complete binary tree from levelorder traversal
+ * 10. Construct of binary tree from postorder and preorder traversal
+ * 11. Construct of binary tree from postorder and preorder traversal (Using hashing)
+ * 12. Construct of binary tree from preorder and preorder traversal of its mirror tree (Using hashing)
+ * 13. Construct ancestor matrix of a binary tree
+ * 14. Construct ancestor matrix of a binary tree (Transitive Closure)
+ * 15. Construct binary tree from preorder traversal and leaf node information
+ * 16. Construct a binary tree from inorder traversal where child nodes are less than parent node.
+ * 17. Construct binary tree from parent array
  */
 public class Construction {
     public static void main(String[] args) {
-         //int[] pre = {1,2,4,5,6,7,3};
-         //int[] post = {4,5,2,6,7,3,1};
-         int[] in = {4,2,5,1,6,3,7};
-         int[] level = {1,2,3,4,5,6,7};
-         BinaryTree binaryTree = constructBinaryTreeLevelInHashing(level, in);
-         binaryTree.levelOrder();
+//         int[] pre = {1,2,4,5,3,6,7};
+//         int[] preM = {1,3,7,6,2,5,4};
+//        LinkedList linkedList = new LinkedList();
+//        linkedList.insertAtEnd(1);
+//        linkedList.insertAtEnd(2);
+//        linkedList.insertAtEnd(3);
+//        linkedList.insertAtEnd(4);
+//        linkedList.insertAtEnd(5);
+//        int level[] = {1,2,3,4,5,6,7};
+//        BinaryTree binaryTree = constructBinaryTreePrePreM(pre, preM);
+//        boolean[][] m = ancestorMatrixTransitiveClosure(binaryTree);
+//        System.out.println();
+//        int[] pre = {10,30,20,5,15};
+//        char[] preC = {'N', 'N', 'L', 'L','L'};
+//        BinaryTree binaryTree = constructTree(pre, preC);
+//        binaryTree.levelOrder();
+        int[] parent = {6,4,4,2,-1,1,1,2};
+        buildTreeFromParentArray(parent).levelOrder();
     }
 
     /**
@@ -280,5 +304,327 @@ public class Construction {
         node.right = buildBinaryTree(level, in, index+1, inEnd);
         return node;
     }
+
+    /**
+     * Construct binary tree from linked list
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeFromLinkedList(LinkedList linkedList) {
+        if(linkedList.head == null) {
+            return null;
+        }
+        Queue<Node> queue = new java.util.LinkedList<>();
+        Node node = new Node(linkedList.head.data);
+        queue.add(node);
+        com.paul.subham.linkedlist.implementation.single.Node current = linkedList.head.next;
+        while(current != null) {
+            Node parent = queue.remove();
+            Node left = new Node(current.data);
+            parent.left = left;
+            current = current.next;
+            queue.add(left);
+            if(current !=null) {
+                Node right = new Node(current.data);
+                parent.right = right;
+                queue.add(right);
+                current = current.next;
+            }
+        }
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = node;
+        return binaryTree;
+    }
+
+    /**
+     * Construct complete binary tree from levelorder traversal
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeFromLevelOrder(int[] level) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = constructBinaryTreeFromLevelOrder(level, 0);
+        return binaryTree;
+    }
+
+    private static Node constructBinaryTreeFromLevelOrder(int[] level, int i) {
+        if(i >= level.length) {
+            return null;
+        }
+        Node node = new Node(level[i]);
+        node.left = constructBinaryTreeFromLevelOrder(level, 2*i+1);
+        node.right = constructBinaryTreeFromLevelOrder(level, 2*i+2);
+        return node;
+    }
+
+    /**
+     * Construct of binary tree from postorder and preorder traversal
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeFromPostPre(int[] post, int[] pre) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = constructBinaryTreeFromPostPre(post, pre, 0, post.length-1);
+        return binaryTree;
+    }
+
+    private static Node constructBinaryTreeFromPostPre(int[] post, int[] pre, int postStart, int postEnd) {
+        if(pIndex == pre.length || postStart > postEnd) {
+            return null;
+        }
+        Node node = new Node(pre[pIndex++]);
+        if(postStart == postEnd || pIndex >= pre.length) {
+            return node;
+        }
+        int postIndex;
+        for(postIndex = postStart; postIndex <=postEnd; postIndex++) {
+            if(pre[pIndex] == post[postIndex]) {
+                break;
+            }
+        }
+        if(postIndex <= postEnd) {
+            node.left = constructBinaryTreeFromPostPre(post, pre, postStart, postIndex);
+            node.right = constructBinaryTreeFromPostPre(post, pre, postIndex+1, postEnd-1);
+        }
+        return node;
+    }
+
+    /**
+     * Construct of binary tree from postorder and preorder traversal (Using hashing)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreeFromPostPreHashing(int[] post, int[] pre) {
+        BinaryTree binaryTree = new BinaryTree();
+        Map<Integer, Integer> postMap = new HashMap<>();
+        for(int i=0; i<post.length; i++) {
+            postMap.put(post[i], i);
+        }
+        binaryTree.root = constructBinaryTreeFromPostPre(pre, 0, post.length-1, postMap);
+        return binaryTree;
+    }
+
+    private static Node constructBinaryTreeFromPostPre(int[] pre, int postStart, int postEnd, Map<Integer, Integer> postMap) {
+        if (pIndex == pre.length || postStart > postEnd) {
+            return null;
+        }
+        Node node = new Node(pre[pIndex++]);
+        if (postStart == postEnd || pIndex >= pre.length) {
+            return node;
+        }
+        int postIndex = postMap.get(pre[pIndex]);
+        node.left = constructBinaryTreeFromPostPre(pre, postStart, postIndex, postMap);
+        node.right = constructBinaryTreeFromPostPre(pre, postIndex + 1, postEnd - 1, postMap);
+        return node;
+    }
+
+    /**
+     * Construct of binary tree from preorder and preorder traversal of its mirror tree (Using hashing)
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructBinaryTreePrePreM(int[] pre, int[] preM) {
+        BinaryTree binaryTree = new BinaryTree();
+        Map<Integer, Integer> preMMap = new HashMap<>();
+        for(int i=0; i<preM.length; i++) {
+            preMMap.put(preM[i], i);
+        }
+        binaryTree.root = constructBinaryTreePrePreM(pre, 0, preM.length-1, preMMap);
+        return binaryTree;
+    }
+
+    private static Node constructBinaryTreePrePreM(int[] pre, int pStart, int pEnd, Map<Integer, Integer> preMMap) {
+        if(pIndex==pre.length || pStart > pEnd) {
+            return null;
+        }
+        Node node = new Node(pre[pIndex++]);
+        if(pStart == pEnd)  {
+            return node;
+        }
+        int index = preMMap.get(pre[pIndex]);
+        node.left = constructBinaryTreePrePreM(pre, index, pEnd, preMMap);
+        node.right = constructBinaryTreePrePreM(pre, pStart+1, index-1, preMMap);
+        return node;
+    }
+
+    /**
+     * Construct ancestor matrix of a binary tree
+     *
+     * a[i][j] = true, if j can be reached from i node.
+     *
+     * TC: O(n^2)
+     * SC: O(n)
+     */
+    public static boolean[][] ancestorMatrix(BinaryTree binaryTree) {
+        boolean[][] matrix = new boolean[10][10];
+        ancestorMatrix(binaryTree.root, matrix, 10);
+        return matrix;
+    }
+
+    private static void ancestorMatrix(Node node, boolean[][] matrix, int n) {
+        if(node == null) {
+            return;
+        }
+        ancestorMatrix(node.left, matrix, n);
+        ancestorMatrix(node.right, matrix, n);
+        if(node.left != null) {
+            matrix[node.data][node.left.data] = true;
+            for(int i=0; i<n; i++) {
+                if(matrix[node.left.data][i]) {
+                    matrix[node.data][i] = true;
+                }
+            }
+        }
+        if(node.right != null) {
+            matrix[node.data][node.right.data] = true;
+            for(int i=0; i<n; i++) {
+                if(matrix[node.right.data][i]) {
+                    matrix[node.data][i] = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Construct ancestor matrix of a binary tree (Transitive Closure)
+     *
+     * a[i][j] = true, if j can be reached from i node.
+     *
+     * TC: O(n^3)
+     * SC: O(n)
+     */
+    public static boolean[][] ancestorMatrixTransitiveClosure(BinaryTree binaryTree) {
+        boolean[][] m = new boolean[10][10];
+        ancestorUtil(binaryTree.root, -1, m);
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++) {
+                for(int k=0; k<10; k++) {
+                    if(m[i][k] && m[k][j]) {
+                        m[i][j] = true;
+                    }
+                }
+            }
+        }
+        return m;
+    }
+
+    private static void ancestorUtil(Node node, int index, boolean[][] m) {
+        if(node == null) {
+            return;
+        }
+        if(index != -1) {
+            m[index][node.data] = true;
+        }
+        ancestorUtil(node.left, node.data, m);
+        ancestorUtil(node.right, node.data, m);
+    }
+
+    /**
+     * Construct binary tree from preorder traversal and leaf node information
+
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree constructTree(int[] pre, char[] preC) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = constructTreeUtil(pre,preC,pre.length);
+        return binaryTree;
+    }
+
+    private static Node constructTreeUtil(int[] pre, char[] preC, int n) {
+        if(pIndex == n) {
+            return null;
+        }
+        Node node = new Node(pre[pIndex++]);
+        if(preC[pIndex-1] == 'N') {
+            node.left = constructTreeUtil(pre, preC, n);
+            node.right = constructTreeUtil(pre, preC, n);
+        }
+        return node;
+    }
+
+    /**
+     * Construct a binary tree from inorder traversal where child nodes are less than parent node.
+
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree buildBinaryTree(int[] in) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = buildTree(in, 0, in.length-1);
+        return binaryTree;
+    }
+
+    private static Node buildTree(int[] in, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+        int maxIndex = max(in, start, end);
+        Node node =  new Node(in[maxIndex]);
+        if(start == end) {
+            return node;
+        }
+        node.left = buildTree(in, start, maxIndex-1);
+        node.right = buildTree(in, maxIndex+1, end);
+        return node;
+    }
+
+    private static int max(int[] a, int start, int end) {
+        int max = a[start];
+        int maxIndex= start;
+        for(int i=start+1; i<=end; i++) {
+            if(a[i] > max) {
+                max = a[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    /**
+     * Construct binary tree from parent array
+     *
+     * Given array that represents a tree in such a way that array indexes are values in tree nodes and array values give
+     * the parent node of that particular index. the value of the root index will be always -1.
+     *
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static BinaryTree buildTreeFromParentArray(int[] parent) {
+        BinaryTree binaryTree = new BinaryTree();
+        Node[] created = new Node[parent.length];
+        for(int i=0; i<parent.length; i++) {
+            if(created[i] == null) {
+                createNode(parent, i, created, binaryTree);
+            }
+        }
+        return binaryTree;
+    }
+
+    public static void createNode(int[] parent, int i, Node[] created, BinaryTree binaryTree) {
+        if(created[i] != null) {
+            return;
+        }
+        created[i] = new Node(i);
+        if(parent[i] == -1) {
+            binaryTree.root = created[i];
+            return;
+        }
+        if(created[parent[i]] == null) {
+            createNode(parent, parent[i], created, binaryTree);
+        }
+        Node p = created[parent[i]];
+        if(p.left == null) {
+            p.left = created[i];
+        } else {
+            p.right = created[i];
+        }
+    }
+
 
 }
