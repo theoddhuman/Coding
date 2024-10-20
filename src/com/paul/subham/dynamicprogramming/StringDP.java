1f2451685b9f5f7ec11a8a85225dc500eab9c87e
Subham Paul <subhampaul137@gmail.com>
2024-10-20 14:32:58 +0530

    string dp completed

--- src/com/paul/subham/dynamicprogramming/StringDP.java
+++ src/com/paul/subham/dynamicprogramming/StringDP.java
@@ -14,6 +14,15 @@ import java.util.Arrays;
  * 9. Minimum insertions to make a string palindrome (Tabulation - Space optimized)
  * 10. Minimum insertions/deletions to convert a string(Tabulation - Space optimized)
  * 11. Shortest Common Supersequence (Tabulation)
+ * 12. Distinct subsequences (Memoization)
+ * 13. Distinct subsequences (Tabulation)
+ * 14. Distinct subsequences (Tabulation - Space optimized)
+ * 15. Edit distance (Memoization)
+ * 16. Edit distance (Tabulation)
+ * 17. Edit distance (Tabulation - space optimized)
+ * 18. Wildcard matching (Memoization)
+ * 19. Wildcard matching (Tabulation)
+ * 20. Wildcard matching (Tabulation - Space optimized)
  */
 public class StringDP {
     public static void main(String[] args) {
@@ -293,5 +302,289 @@ public class StringDP {
             j--;
         }
         return sb.toString();
+    }
+
+    /**
+     * Distinct subsequences (Memoization)
+     *
+     * Given two strings s and t, return the number of distinct subsequences of s which equals t
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<=m;i++) {
+            Arrays.fill(dp[i],-1);
+        }
+        return ways(s,t,m,n,dp);
+    }
+
+    private static int ways(String s, String t, int i, int j, int[][] dp) {
+        if(j==0) {
+            return 1;
+        }
+        if(i==0) {
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s.charAt(i-1) == t.charAt(j-1)) {
+            return dp[i][j] = ways(s,t,i-1,j-1,dp) + ways(s,t,i-1,j,dp);
+        }
+        return dp[i][j] = ways(s,t,i-1,j,dp);
+    }
+
+    /**
+     * Distinct subsequences (Tabulation)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int numDistinctTab(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<=m;i++) {
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * Distinct subsequences (Tabulation - Space optimized)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int numDistinctTabSpaceOpt(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[] pre = new int[n+1];
+        int[] cur = new int[n+1];
+        pre[0] = cur[0] = 1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    cur[j] = pre[j] + pre[j-1];
+                } else {
+                    cur[j] = pre[j];
+                }
+            }
+            System.arraycopy(cur,0,pre,0,n+1);
+        }
+        return pre[n];
+    }
+
+    /**
+     * Edit distance (Memoization)
+     *
+     * We are given two strings ‘S1’ and ‘S2’. We need to convert S1 to S2. The following three operations are allowed:
+     * 1. Deletion of a character.
+     * 2. Replacement of a character with another one.
+     * 3. Insertion of a character.
+     *
+     * When the last characters of strings don’t match. Make three recursive calls as show below:
+     *
+     * Insert s2[n-1] at last of s1 : editDist(m, n-1)
+     * Replace s1[m-1] with s2[n-1] : editDist(m-1, n-1)
+     * Remove s1[m-1] : editDist(m-1, n)
+     *
+     * TC: O(3^n)
+     * SC: O(n)
+     */
+    private static int editDistanceMemoization(String s1, String s2) {
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i=0; i<=s1.length(); i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return editDistance(s1, s2, s1.length(), s2. length(), dp);
+    }
+
+    private static int editDistance(String s1, String s2, int m, int n, int[][] dp) {
+        if(m == 0) {
+            return n;
+        }
+        if(n == 0) {
+            return m;
+        }
+        if(dp[m][n] != -1) {
+            return dp[m][n];
+        }
+        if (s1.charAt(m-1) == s2.charAt(n-1)) {
+            return dp[m][n] = editDistance(s1, s2, m-1, n-1, dp);
+        }
+        return dp[m][n] = 1 + Math.min(editDistance(s1, s2, m-1, n, dp), Math.min(editDistance(s1, s2, m-1, n-1, dp), editDistance(s1, s2, m, n-1, dp)));
+    }
+
+    /**
+     * Edit distance (Tabulation)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int editDistanceTabulation(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<=m; i++) {
+            dp[i][0] = i;
+        }
+        for(int i=0; i<=n; i++) {
+            dp[0][i] = i;
+        }
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * Edit distance (Tabulation - space optimized)
+     *
+     * TC: O(mn)
+     * SC: O(n)
+     */
+    public static int editDistanceTabulationSpaceOptimized(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[] curr = new int[n+1];
+        int[] pre = new int[n+1];
+        for(int i=0; i<=n; i++) {
+            pre[i] = i;
+        }
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    curr[j] = pre[j-1];
+                } else {
+                    curr[j] = 1 + Math.min(curr[j-1], Math.min(pre[j], pre[j-1]));
+                }
+            }
+            System.arraycopy(curr,0,pre,0,n+1);
+        }
+        return pre[n];
+    }
+
+    /**
+     * Wildcard matching (Memoization)
+     *
+     * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+     * '?' Matches any single character.
+     * '*' Matches any sequence of characters (including the empty sequence)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        Boolean[][] dp = new Boolean[m+1][n+1];
+        return isMatch(s,p,m,n,dp);
+    }
+
+    private static boolean isMatch(String s, String p, int i, int j, Boolean[][] dp) {
+        if(i==0 && j==0) {
+            return true;
+        }
+        if(j==0) {
+            return false;
+        }
+        if(i==0) {
+            return isAllStars(p, j);
+        }
+        if(dp[i][j] != null) {
+            return dp[i][j];
+        }
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') {
+            return dp[i][j] = isMatch(s,p,i-1,j-1,dp);
+        }
+        if(p.charAt(j-1) == '*') {
+            return dp[i][j] = isMatch(s,p,i,j-1,dp) || isMatch(s,p,i-1,j,dp);
+        }
+        return dp[i][j] = false;
+    }
+
+    private static boolean isAllStars(String s, int end) {
+        for(int i=0; i<end; i++) {
+            if(s.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Wildcard matching (Tabulation)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static boolean isMatchTab(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for(int i=1; i<=n; i++) {
+            dp[0][i] = isAllStars(p,i);
+        }
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1) == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if(p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * Wildcard matching (Tabulation - Space optimized)
+     *
+     * TC: O(mn)
+     * SC: O(n)
+     */
+    public static boolean isMatchTabSpaceOpt(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[] pre = new boolean[n+1];
+        boolean[] cur = new boolean[n+1];
+        pre[0] = true;
+        for(int i=1; i<=n; i++) {
+            pre[i] = isAllStars(p,i);
+        }
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1) == '?') {
+                    cur[j] = pre[j-1];
+                } else if(p.charAt(j-1) == '*') {
+                    cur[j] = pre[j] || cur[j-1];
+                } else {
+                    cur[j] = false;
+                }
+            }
+            System.arraycopy(cur,0,pre,0,n+1);
+        }
+        return pre[n];
     }
 }
