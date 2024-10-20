65f49836b1816ed2a4365bafbf8b08b5478ccef4
Subham Paul <subhampaul137@gmail.com>
2024-10-20 12:24:28 +0530

    longest common substring

--- src/com/paul/subham/dynamicprogramming/StringDP.java
+++ src/com/paul/subham/dynamicprogramming/StringDP.java
@@ -7,10 +7,13 @@ import java.util.Arrays;
  * 2. Longest common subsequence (Tabulation)
  * 3. Longest common subsequence (Tabulation - Space optimized)
  * 4. Print longest common subsequence (Tabulation)
+ * 5. Longest common substring (Memoization)
+ * 6. Longest common substring (Tabulation)
+ * 7. Longest common substring (Tabulation - Space optimized)
  */
 public class StringDP {
     public static void main(String[] args) {
-        System.out.println(printLongestCommonSubsequence("abcde", "acd"));
+        System.out.println(longestCommonSubstring("abcdgh", "acdghr"));
     }
 
     /**
@@ -122,5 +125,89 @@ public class StringDP {
            }
         }
         return s.toString();
+    }
+
+    /**
+     * Longest common substring (Memoization)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int longestCommonSubstringMem(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<=m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        lcSubstring(s1,s2,m,n,dp);
+        return max;
+    }
+    private static int max = 0;
+    private static int lcSubstring(String s1, String s2, int i, int j, int[][] dp) {
+        if(i==0 || j==0) {
+            return 0;
+        }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = 0;
+        if(s1.charAt(i-1) == s2.charAt(j-1)) {
+            ans = 1 + lcSubstring(s1, s2, i-1, j-1, dp);
+            max = Math.max(ans, max);
+        }
+        //these two recursive calls are mandatory, otherwise we may miss a few substring matching if we return from above if condition,
+        // ("yxxzzxxxx", "yzyzxxyxxz")
+        lcSubstring(s1, s2, i-1, j,dp);
+        lcSubstring(s1, s2, i, j-1,dp);
+        return dp[i][j] = ans;
+    }
+
+    /**
+     * Longest common substring (Tabulation)
+     *
+     * TC: O(mn)
+     * SC: O(mn)
+     */
+    public static int longestCommonSubstringTab(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        int max = 0;
+        for(int i=1; i<=m;i++){
+            for(int j=1; j<=n; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Longest common substring (Tabulation - Space optimized)
+     *
+     * TC: O(mn)
+     * SC: O(n)
+     */
+    public static int longestCommonSubstring(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[] pre = new int[n+1];
+        int[] cur = new int[n+1];
+        int max = 0;
+        for(int i=1; i<=m;i++){
+            for(int j=1; j<=n; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    cur[j] = 1 + pre[j-1];
+                    max = Math.max(cur[j], max);
+                } else {
+                    cur[j] = 0;
+                }
+            }
+            System.arraycopy(cur, 0, pre, 0, n+1);
+        }
+        return max;
     }
 }
